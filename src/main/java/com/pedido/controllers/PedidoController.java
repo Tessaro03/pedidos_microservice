8190b8d930ce8856e1803a7aec6ba96b8dee2bc0
Gabriel Tessaro Favareto <gabrieltessarofavareto@gmail.com>
2024-07-13 17:19:08 -0300

    Adicionado anotações para o Swagger

--- src/main/java/com/pedido/controllers/PedidoController.java
+++ src/main/java/com/pedido/controllers/PedidoController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pedido.dtos.PedidoInputDTO;
 import com.pedido.service.PedidoService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 
@@ -29,27 +29,26 @@ public class PedidoController {
 
     
     @GetMapping
+    @Operation(summary = "Ver Pedidos", description = "Retorna os pedidos criados")
     public ResponseEntity verPedidos(){
         return ResponseEntity.ok().body(service.verPedidos());
     }
 
 
     @PostMapping
+    @Operation(summary = "Criar Pedido", description = "Cria o pedido")
     public void criarPedido(@RequestBody @Valid PedidoInputDTO pedido){
         service.criar(pedido);
     }
 
     @PatchMapping("/{idPedido}/confirmado")
+    @Operation(summary = "Confirmar Pedido", description = "Confirma o pedido e envia msg para pagamentos criar um pagamento")
     public void confirmarPedido(@PathVariable Long idPedido){
         service.confirmarPedido(idPedido);
     }
 
-    @PutMapping("/{idPedido}/pago")
-    public void pagamentoConfirmado(@PathVariable Long idPedido){
-        service.pagamentoConfirmado(idPedido);
-    }
-
     @DeleteMapping("/{idPedido}")
+    @Operation(summary = "Deleta Pedido", description = "Deleta o pedido e envia msg para pagamentos cancelar o pagamento")
     public void cancelarPedido(@PathVariable long idPedido){
         service.cancelarPedido(idPedido);
     }
